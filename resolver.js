a19568edb9e7619765a1c385c6d60cd2d706ce34
MahardiikaKD <dikamahar884@gmail.com>
2024-11-09 22:35:05 +0700

    init

---
+++ resolver.js
@@ -0,0 +1,43 @@
+import { getJobs, getJob, getJobsByCompany } from "./db/jobs.js";
+import { getCompany } from './db/companies.js';
+import { GraphQLError } from "graphql";
+
+export const resolvers = {
+    Query: {
+        greeting: () => 'Hello World',
+        jobs: async () => {
+            return await getJobs();
+        },
+        job: async (_root, args) => {
+            return await getJob(args?.id);
+        },
+        company: async (_root, args) => {
+            const company = await getCompany(args?.id);
+
+            if (!company) {
+                throw notFoundError('No Company found with id : ' +  args.id);
+            }
+
+            return company;
+        }
+    },
+
+    Company: {
+        jobs: async (company) => await getJobsByCompany(company.id)
+    },
+
+    Job: {
+        company: (job) => getCompany(job.companyId),
+        date: (job) => toISOString(job.createdAt)
+    }
+    
+};
+
+const notFoundError = (message) => {
+    return new GraphQLError(message, {
+        extensions: {
+            code: 'NOT_FOUND'
+        }
+    })
+}
+const toISOString = (date) => date.slice(0, 'yyyy-mm-dd'.length);
